65555800aae9b705e4e6e21424ba9cd30f123c27
sepehr javid <javid.sepehr77@gmail.com>
2022-02-17 18:04:00 +0200

    update socket to websocket

--- client.js
+++ client.js
@@ -1,13 +1,8 @@
 import Session from "./session/session.js";
-import tls from 'tls';
 import fs from 'fs';
 
 // TODO: Later to be replaced with enc layer function
-var ssl_context = tls.createSecureContext({ca: [ fs.readFileSync('./cert/cert.pem') ]})
+const ssl_context = {ca: fs.readFileSync('./cert/cert.pem')}
 
 // Session layer
-var new_session = new Session("127.0.0.1", 5555, ssl_context);
-var data_from_server = await new_session.recv_data();
-console.log("From server: "+data_from_server.toString());
-new_session.send_data("Hello server, This is client!");
-new_session.close_connection();
+const new_session = new Session("wss://SEP:5555", ssl_context);
--- session/session.js
+++ session/session.js
@@ -1,14 +1,8 @@
-import { waitForDebugger } from 'inspector';
-import net from 'net';
-import { ThrowStatement } from 'requirejs';
-import tls from 'tls';
+import WebSocket from 'ws';
 
 class Session {
 
-    #client_socket;
-    #client_ssl_socket;
-    #received_data;
-    #data_available = false;
+    client_websocket;
 
     /**
      * Creates a net socket and wraps it around tls using tls module. It needs tls.SecureContext from encryption layer.
@@ -16,105 +10,52 @@ class Session {
      * @param {*} port_number Port number to connect to
      * @param {*} ssl_context tls.SecureContext object that is passed to session layer from the encryption layer
      */
-    constructor(ip_address, port_number, ssl_context) {
-        try{
-            console.log("INFO: Client will attempt connection to ip: "+ip_address + ", port: " + port_number)
-            
-            // Create client socket.
-            this.#client_socket = net.createConnection({port: port_number, host: ip_address});
+    constructor(uri, ssl_context, on_message, on_open) {
+        try {
+            console.log(`INFO: Client will attempt connection to ${uri}`)
 
-            // warp the socket around tls.
-            this.#client_ssl_socket = tls.connect({
-                socket: this.#client_socket, 
-                servername: "SEP", 
-                secureContext: ssl_context
-                },
-                ()=>{
-                    // Must check if socket authorized manually.
-                    console.log("INFO: Secure connection established with server.")
-                    if(!this.#client_ssl_socket.authorized){
-                        console.error("ERROR: Socket was not authorized, could not determine if the server certificate was signed by one of the specified CAs.");
-                        console.error("ERROR: "+this.#client_ssl_socket.authorizationError);
-                        this.#client_ssl_socket.end();
-                    }else{
-                        console.log("INFO: Secure Socket was authorized successfully using certificate.");
-                    }
-                },
-                );
+            // Create client websocket.
+            this.client_websocket = new WebSocket(uri, ssl_context);
 
-            // LISTENERS
+            this.client_websocket.addEventListener("message", ({data}) => {
+                console.log(data);
+                this.send_data({"message": "client"})
+            });
+            this.client_websocket.addEventListener("open", () => {
+            });
+            this.client_websocket.addEventListener("close", () => {
+                console.log("closed")
+            });
+            this.client_websocket.addEventListener("error", this.on_error);
 
-            // Listener for incoming data.
-            this.#client_ssl_socket.addListener('data', (data)=>{
-                console.log("INFO: Listener received data from server.");
-                //console.log(data.toString());
-                this.#received_data = data;
-                this.#data_available = true;
-            })
-
-            // Listener for closing the socket if server closes first.
-            this.#client_ssl_socket.addListener('end', (data)=>{
-                console.log("INFO: Server closed socket. Closing client socket...");
-                this.#client_ssl_socket.end();
-            })
-            
-            // Listener for catching networking errors.
-            this.#client_ssl_socket.addListener('error', (error)=>{
-                console.log("INFO: Listener caught an error...");
-                console.error("ERROR: "+error);
-            })
-
-            // Listener for catching tls errors.
-            this.#client_ssl_socket.addListener('tlsClientError', (tls_error)=>{
-                console.log("INFO: Listener caught a tls error...");
-                console.error("ERROR: "+tls_error);
-            })            
-                        
-            
-            //console.log(this.#client_ssl_socket);
-        }catch(error){
+        } catch (error) {
             console.error(error);
         }
     }
 
     /**
      * Sends data to server when the socket is ready.
-     * @param {*} data 
+     * @param {*} data
      * @returns boolean true if all data is flushed to kernel socket.
      */
-    send_data(data){
-        // console.log(this.#client_ssl_socket.connecting)
-        try{
-            return this.#client_ssl_socket.write(data);
-        }catch(error){
+    send_data(data) {
+        try {
+            return this.client_websocket.send(JSON.stringify(data))
+        } catch (error) {
             console.error(error);
         }
     }
 
     /**
-     * receives data from server.
-     * @returns a promise that might contain data received from socket listener.
-     */
-    recv_data() {
-        return new Promise( (resolve, reject)=>{
-            // Wait 200 ms then try returning data received from socket listener.
-            setTimeout(()=>{
-                // resolve data.
-                if(this.#data_available){
-                    this.#data_available = false;
-                    resolve(this.#received_data);
-                }else{
-                    reject("Error: promise timed out before receiving any data.");
-                }
-            }, 200)
-        })
-    }
-
-    /**
      * Closes the connection to the server.
      */
-    close_connection(){
-        this.#client_ssl_socket.end();
+    close_connection() {
+        this.client_websocket.close();
+    }
+
+
+    on_error(event) {
+        console.log(event)
     }
 
 }
