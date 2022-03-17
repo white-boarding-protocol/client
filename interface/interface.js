c103547a226fa6c0fed710130ff4b2ba2af50c39
sepehr javid <javid.sepehr77@gmail.com>
2022-03-17 23:27:15 +0200

    add connect and create room methods to the interface and alter on message mechanism,

--- interface/interface.js
+++ interface/interface.js
@@ -1,26 +1,36 @@
 import fs from "fs";
-import Whiteboarding from "../whiteboarding/whiteboarding";
-import { v4 as uuidv4 } from 'uuid';
+import Whiteboarding from "../whiteboarding/whiteboarding.js";
+import {v4 as uuidv4} from 'uuid';
 
 class Interface {
 
     constructor(userID, uri, cert_path, on_close) {
         this.userID = userID;
         this.whiteboarding = new Whiteboarding(userID, uri, {ca: fs.readFileSync(cert_path)}, () => on_close());
+        this.errorMsg = null
+    }
+
+    async connect() {
+        return await this.whiteboarding.start()
     }
 
     async createRoom() {
-        
         let uuid = uuidv4();
 
-        await this.whiteboarding.session.client_websocket.send(JSON.stringify({"type": 1, "room_event_type": 0, 
-        "user_id": this.userID, "uuid": uuid}));
+        await this.whiteboarding.session.client_websocket.send(JSON.stringify({
+            "type": 1,
+            "room_event_type": 0,
+            "user_id": this.userID,
+            "uuid": uuid
+        }));
 
-        let myFirstPromise = new Promise();
 
-        this.whiteboarding.storage[uuid] = myFirstPromise;
-        
-        return myFirstPromise
+        return new Promise((resolve, reject) => {
+            this.whiteboarding.storage[uuid] = {
+                res: (msg) => resolve(msg),
+                rej: (msg) => reject(msg)
+            }
+        })
     }
 
     async Draw() {
--- whiteboarding/whiteboarding.js
+++ whiteboarding/whiteboarding.js
@@ -1,5 +1,6 @@
 import Session from "../session/session.js";
-import Interface from "../interface/interface";
+import {v4 as uuidv4} from "uuid";
+
 
 class Whiteboarding {
     session;
@@ -12,80 +13,62 @@ class Whiteboarding {
         this.uri = uri
         this.on_close = on_close
         this.userID = userID;
-        this.storage = {
-            uuid: Promise
-        }
-
+        this.isConnected = false
+        this.storage = {}
     }
 
     async start() {
-        this.session = new Session(this.uri, this.ssl_context, (e) => this.on_message(e), () => this.on_open(), this.on_close);
+        let uuid = uuidv4()
+        let promise = new Promise((resolve, reject) => {
+            this.storage[uuid] = {
+                res: (msg) => resolve(msg),
+                rej: (msg) => reject(msg),
+                promise: null
+            }
+        }).then(() => {
+            this.isConnected = true
+        }).catch((reason) => {
+            this.isConnected = false
+            this.errorMsg = reason
+        })
+
+        this.storage[uuid].promise = promise
+        this.session = new Session(this.uri, this.ssl_context, (e) => this.on_message(e),
+            () => this.on_open(uuid), this.on_close);
+        return promise
     }
 
-    async on_open(event) {
-        await this.session.client_websocket.send(JSON.stringify({"user_id": this.userID, "message": "hello"}));
+    async on_open(uuid) {
+        await this.session.client_websocket.send(JSON.stringify({
+            "user_id": this.userID,
+            "message": "hello",
+            "uuid": uuid
+        }));
     }
 
     async on_message(event) {
         let data_obj = JSON.parse(event.data);
-        switch (data_obj.status[0]) {
-            case '1':
-                this.whiteboarding.storage[uuid].resolve("success")
-                await this.handle_user_events(data_obj);
+        console.log(data_obj)
+        switch (data_obj.status) {
+            case 200:
+                this.storage[data_obj.uuid].res(data_obj.message)
                 break;
-            case '2':
-                await this.handle_host_events(data_obj);
+            case 301:
+                // user in queue
                 break;
-            case '3':
+            case 300:
+                //redist event
                 await this.handle_redistributed_events(data_obj);
                 break;
-            case '4':
-                await this.handle_error(data_obj);
+            case 400:
+                this.storage[data_obj.uuid].rej(data_obj.message)
+                break;
+            case 403:
+                this.storage[data_obj.uuid].rej(data_obj.message)
                 break;
         }
     }
 
-
-    async handle_user_events(data_obj) {
-        switch (data_obj.status) {
-            case '100':
-                // for user connect
-                break;
-            case '199':
-                // user disconnect
-                break;
-            case '101':
-                // joined room
-                break;
-            case '102':
-                // for join request is here for host
-                break;
-            case '103':
-                // join request successfully sent
-                break;
-            case '104':
-                // left room
-                break;
-        }
-    }
-
-    async handle_host_events(data_obj) {
-
-        switch (data_obj.status) {
-            case '201':
-                // create room 201
-                break;
-            case '202':
-                // accept user 202
-                break;
-            case '203':
-                // decline user 203
-                break;
-            case '200':
-                // end room 200
-                break;
-        }
-    }
 
     async handle_redistributed_events(data_obj) {
 
